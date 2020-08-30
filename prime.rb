71b840b75c2034edee274b3df09580683a117672
Anthony Hernandez <anthonyh2020@gmail.com>
2020-08-30 06:07:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,4 +3,10 @@ def prime(numbers)
   if numbers <= 1
     return false
   elsif (2..numbers - 1).each do |number|
-    if numbers%
+    if numbers % number = 0 
+      return false
+    end
+  else
+    return true
+  end
+end

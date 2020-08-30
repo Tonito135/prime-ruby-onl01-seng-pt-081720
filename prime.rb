e5fe8a67f57d50dd8dea676334ab9c285a3c5ab7
Anthony Hernandez <anthonyh2020@gmail.com>
2020-08-30 06:16:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,12 @@
 def prime?(numbers)
   if numbers <= 1 || numbers == 0 || numbers == 1
     return false
-  elsif (2..numbers - 1).each do |number|
-    if numbers % number == 0 
-      return false
+  elsif 
+    (2..numbers - 1).each do |number|
+      if numbers % number == 0 
+        return false
+      end
     end
   end
+  true
 end

04c6cf2594392d71d18d3a52a3ec1ac51496b0c8
Anthony Hernandez <anthonyh2020@gmail.com>
2020-08-30 06:10:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,11 +2,12 @@ array = []
 def prime(numbers)
   if numbers <= 1 || numbers == 0 || numbers == 1
     return false
-  elsif (2..numbers - 1).each do |number|
-    if numbers % number = 0 
-      return false
+  elsif 
+    (2..numbers - 1).each do |number|
+      if numbers % number = 0 
+        return false
+      end
     end
-  end
   else
     return true
   end
